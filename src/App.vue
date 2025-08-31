ce47fd07f2a7f7961ac291b6be7be851554b96e2
JinYunhwa <dbsghk320@gmail.com>
2025-08-31 19:36:19 +0900

    할 일 완료 기능 추가 (체크박스)

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,17 @@
         v-for="todo in todos"
         :key="todo.id"
         class="list-group-item d-flex justify-content-between align-items-center">
+        <div class="d-flex align-items-center gap-2">
+          <input
+            type="checkbox"
+            class="form-check-input"
+            :checked="todo.completed"
+            @change="toggleTodo(todo.id)"
+          />
+        </div>
+        <span :class="{ 'text-decoration-line-through text-secondary': todo.completed }">
           {{ todo.title }}
+        </span>
         <button
           class="btn btn-sm btn-outline-danger"
           @click="removeTodo(todo.id)"
@@ -51,5 +61,12 @@ function addTodo() {
 
 function removeTodo(id) {
   todos.value = todos.value.filter(todo => todo.id !== id)
+}
+
+function toggleTodo(id) {
+  const todo = todos.value.find(t => t.id === id)
+  if (todo) {
+    todo.completed = !todo.completed
+  }
 }
 </script>
